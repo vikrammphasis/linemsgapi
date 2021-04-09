51a4a4e6755ffad05a7d05817be0c2b1d47e104c
vikram <vikram.aroskar@gmail.com>
2021-04-09 17:42:58 +0530

    update

--- src/main/java/com/vikram/linemsgapi/LinemsgapiApplication.java
+++ src/main/java/com/vikram/linemsgapi/LinemsgapiApplication.java
@@ -1,15 +1,21 @@
 package com.vikram.linemsgapi;
 
+import java.util.concurrent.ExecutionException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.PushMessage;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
@@ -17,11 +23,39 @@ import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 @LineMessageHandler
 public class LinemsgapiApplication {
 	
+	
 	private final Logger log = LoggerFactory.getLogger(LinemsgapiApplication.class);
 
+//	@Value( "${line.bot.channel-token}" )
+//	static private String token;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LinemsgapiApplication.class, args);
+		
+		sendMessageToClient();
+	}
+
+	private static void sendMessageToClient() {
+		final LineMessagingClient client = LineMessagingClient
+		        .builder("6gQtTrQIysvNg1C/39SY9Jv8wJZtmbTE2XiKSlFAww4QtFb0ZpTh99TgeT3dmFSaTxpY2ie7JxhD9GKKUxt95vY2TrGfIH+NclfCIA09Uh6TJoOHv2WkLeGLpByN8stwRUofkCZx75Wyzqt0mwU8EwdB04t89/1O/w1cDnyilFU=")
+		        .build();
+
+		final TextMessage textMessage = new TextMessage("hello");
+		final PushMessage pushMessage = new PushMessage(
+		        "<to>",
+		        textMessage);
+
+		final BotApiResponse botApiResponse;
+		try {
+		    botApiResponse = client.pushMessage(pushMessage).get();
+		} catch (InterruptedException | ExecutionException e) {
+		    e.printStackTrace();
+		    return;
+		}
+
+		System.out.println(botApiResponse);
+
+		
 	}
 
 	@EventMapping
