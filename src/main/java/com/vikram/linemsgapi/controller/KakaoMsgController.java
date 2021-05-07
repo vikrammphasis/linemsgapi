f442982fc69bf6269ad305c6899752ea8a30e054
vikram <vikram.aroskar@gmail.com>
2021-05-07 14:23:48 +0530

    trying kakao token

--- src/main/java/com/vikram/linemsgapi/controller/KakaoMsgController.java
+++ src/main/java/com/vikram/linemsgapi/controller/KakaoMsgController.java
@@ -1,11 +1,14 @@
 package com.vikram.linemsgapi.controller;
 
+import java.net.URI;
 import java.util.Arrays;
 
+import org.json.simple.JSONObject;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,24 +26,63 @@ public class KakaoMsgController {
                "}";
     }
 
-    @RequestMapping(value = "/kakaotest")
-    public String getPostResponse(){
+    @RequestMapping(value = "/kakaooauth", method = RequestMethod.POST)
+    public String kakaooauth(@RequestBody String body){
     	
-    	String token = "";
-    	RestTemplate restTemplate = new RestTemplate();
-
-    	HttpHeaders headers=new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        
-        //kakao
-        headers.setBearerAuth(token);
-        
-        HttpEntity<String> entity=new HttpEntity<String>(headers);
-        
-        //return restTemplate.exchange("http://localhost:8080/postdata",HttpMethod.POST,entity,String.class).getBody();
-
+    	
+    	System.out.println("body in kakaooauth api");
+		System.out.println(body);
+		
+		return "";
+//    	
+//    	String token = "";
+//    	RestTemplate restTemplate = new RestTemplate();
+//
+//    	HttpHeaders headers=new HttpHeaders();
+//        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+//        
+//        //kakao
+//        headers.setBearerAuth(token);
+//        
+//        HttpEntity<String> entity=new HttpEntity<String>(headers);
+//        
+//        //return restTemplate.exchange("http://localhost:8080/postdata",HttpMethod.POST,entity,String.class).getBody();
+//
+//    
+//        return restTemplate.exchange("https://kapi.kakao.com/v1/api/talk/profile",HttpMethod.GET,entity,String.class).getBody();
+    }
     
-        return restTemplate.exchange("https://kapi.kakao.com/v1/api/talk/profile",HttpMethod.GET,entity,String.class).getBody();
+    
+
+    @RequestMapping(value = "/kakaotoken")
+    public String kakaotoken(){
+    	
+    	RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		
+		
+    	String token_url = "https://kauth.kakao.com/oauth/token";
+    	
+    	JSONObject requestJsonObject = new JSONObject();
+		requestJsonObject.put("grant_type","authorization_code");
+		requestJsonObject.put("client_id", "e8cc98f63ab405693506084198130566");
+		requestJsonObject.put("code", "HgX-MBllWujA0UCMputwyCY1_VL_xbWibhaNGiya5V8yN-MueAllD3JOsS-KZ9JMvEzjQQo9cpgAAAF5RS7hJw");
+	
+		
+		HttpEntity<String> request = new HttpEntity<String>(requestJsonObject.toString(), headers);
+		
+		URI locationHeader = restTemplate.postForLocation(token_url, request);
+		
+		System.out.println("locationHeader in kakao token api");
+		System.out.println(locationHeader);
+
+		
+		String resultAsJsonStr = restTemplate.postForObject(token_url, request, String.class);
+		
+		System.out.println("resultAsJsonStr>>" + resultAsJsonStr);
+    	
+    	return "";
     }
 
 }
--- src/main/java/com/vikram/linemsgapi/controller/ViberController.java
+++ src/main/java/com/vikram/linemsgapi/controller/ViberController.java
@@ -1,9 +1,7 @@
 package com.vikram.linemsgapi.controller;
 
 import java.net.URI;
-import java.util.List;
 
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.http.HttpEntity;
@@ -14,31 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import com.zendesk.sunshine_conversations_client.ApiClient;
-import com.zendesk.sunshine_conversations_client.ApiException;
-import com.zendesk.sunshine_conversations_client.Configuration;
-import com.zendesk.sunshine_conversations_client.api.ConversationsApi;
-import com.zendesk.sunshine_conversations_client.api.IntegrationsApi;
-import com.zendesk.sunshine_conversations_client.api.MessagesApi;
-import com.zendesk.sunshine_conversations_client.api.ParticipantsApi;
-import com.zendesk.sunshine_conversations_client.api.UsersApi;
-import com.zendesk.sunshine_conversations_client.api.WebhooksApi;
-import com.zendesk.sunshine_conversations_client.auth.HttpBasicAuth;
-import com.zendesk.sunshine_conversations_client.model.Author;
-import com.zendesk.sunshine_conversations_client.model.ConversationListFilter;
-import com.zendesk.sunshine_conversations_client.model.ConversationListResponse;
-import com.zendesk.sunshine_conversations_client.model.Integration;
-import com.zendesk.sunshine_conversations_client.model.IntegrationListFilter;
-import com.zendesk.sunshine_conversations_client.model.IntegrationListResponse;
-import com.zendesk.sunshine_conversations_client.model.MessagePost;
-import com.zendesk.sunshine_conversations_client.model.MessagePostResponse;
-import com.zendesk.sunshine_conversations_client.model.Page;
-import com.zendesk.sunshine_conversations_client.model.TextMessage;
-import com.zendesk.sunshine_conversations_client.model.UserResponse;
-import com.zendesk.sunshine_conversations_client.model.WebhookCreateBody;
-import com.zendesk.sunshine_conversations_client.model.WebhookListResponse;
-import com.zendesk.sunshine_conversations_client.model.WebhookResponse;
 
 @RestController
 public class ViberController {
