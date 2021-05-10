e9f7ba8ee24d2658b30deee0fed169ae2a5c90cc
vikram <vikram.aroskar@gmail.com>
2021-05-10 10:48:43 +0530

    addd hub.challenge from request param to GET

--- src/main/java/com/vikram/linemsgapi/controller/FacebookMessengerController.java
+++ src/main/java/com/vikram/linemsgapi/controller/FacebookMessengerController.java
@@ -1,5 +1,7 @@
 package com.vikram.linemsgapi.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,16 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class FacebookMessengerController {
 
 	@RequestMapping(value = "/facebookwebhook", method = RequestMethod.GET)
-	public String facebookMessagesGetResponse() {
+	public String facebookMessagesGetResponse(HttpServletRequest request) {
 
 		String token = "EAAN7FuBZC4OUBAFGjanrbHBDTMQ085DyhiZBOwOWe4ULD8nGPAj7d0sJNXauuNUglh1N8DUTF14C8NdjQWjwEU6RI5FakkK3nynHH1uZADXGaQS24lOkdbDuZCJX8oSTir8qFQdk4hU8eGmsMthj2UEXockuwlZBPrCrUIIPiJ9yGZAx2aSukrNzH393cn4HoZD";
 		String VERIFY_TOKEN = "aee8385a-7544-4a97-b751-5334e7ee5178";
 
 		System.out.println("facebookwebhook GET body");
 		
+		
+		String hub_challenge = request.getParameter("hub.challenge");
 
+		System.out.println("hub_challenge " + hub_challenge);
 		//hub.challenge
-		return "132337777";
+		return hub_challenge;
 	}
 
 	@RequestMapping(value = "/facebookwebhook", method = RequestMethod.POST)
