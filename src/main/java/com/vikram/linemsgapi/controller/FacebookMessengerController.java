c795f9ab3b93dc8877a7c231268a082380519ee9
vikram <vikram.aroskar@gmail.com>
2021-05-10 10:37:15 +0530

    trying kakao token

--- src/main/java/com/vikram/linemsgapi/controller/FacebookMessengerController.java
+++ src/main/java/com/vikram/linemsgapi/controller/FacebookMessengerController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FacebookMessengerController {
 
 	@RequestMapping(value = "/facebookwebhook", method = RequestMethod.GET)
-	public String facebookMessagesGetResponse(@RequestBody String body) {
+	public String facebookMessagesGetResponse() {
 
 		String token = "EAAN7FuBZC4OUBAFGjanrbHBDTMQ085DyhiZBOwOWe4ULD8nGPAj7d0sJNXauuNUglh1N8DUTF14C8NdjQWjwEU6RI5FakkK3nynHH1uZADXGaQS24lOkdbDuZCJX8oSTir8qFQdk4hU8eGmsMthj2UEXockuwlZBPrCrUIIPiJ9yGZAx2aSukrNzH393cn4HoZD";
 		String VERIFY_TOKEN = "aee8385a-7544-4a97-b751-5334e7ee5178";
