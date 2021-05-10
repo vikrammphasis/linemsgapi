bd95923a610e984fd2bde11371a2324b0c6b7ee5
vikram <vikram.aroskar@gmail.com>
2021-05-10 10:36:22 +0530

    add faccebook webhook POST and GET

--- src/main/java/com/vikram/linemsgapi/controller/FacebookMessengerController.java
+++ src/main/java/com/vikram/linemsgapi/controller/FacebookMessengerController.java
@@ -7,28 +7,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FacebookMessengerController {
-	
-	
+
 	@RequestMapping(value = "/facebookwebhook", method = RequestMethod.GET)
 	public String facebookMessagesGetResponse(@RequestBody String body) {
-		
+
+		String token = "EAAN7FuBZC4OUBAFGjanrbHBDTMQ085DyhiZBOwOWe4ULD8nGPAj7d0sJNXauuNUglh1N8DUTF14C8NdjQWjwEU6RI5FakkK3nynHH1uZADXGaQS24lOkdbDuZCJX8oSTir8qFQdk4hU8eGmsMthj2UEXockuwlZBPrCrUIIPiJ9yGZAx2aSukrNzH393cn4HoZD";
 		String VERIFY_TOKEN = "aee8385a-7544-4a97-b751-5334e7ee5178";
-		
+
 		System.out.println("facebookwebhook GET body");
 		System.out.println(body);
-		
+
 		return "";
 	}
-	
-	
+
 	@RequestMapping(value = "/facebookwebhook", method = RequestMethod.POST)
 	public String facebookMessagesPostResponse(@RequestBody String body) {
-		
+
 		String VERIFY_TOKEN = "aee8385a-7544-4a97-b751-5334e7ee5178";
-		
+
 		System.out.println("facebookwebhook POST body");
 		System.out.println(body);
-		
+
 		return "";
 	}
 }
